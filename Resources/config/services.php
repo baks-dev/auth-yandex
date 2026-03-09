36b7a6f037597cb02df05ed4ea8a4883f4a18267
MichelAngelo <baksdevelopmen@gmail.com>
2026-03-10 01:15:05 +0300

    BaksDevelopment

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -30,8 +30,7 @@ return static function(ContainerConfigurator $configurator) {
     $services = $configurator->services()
         ->defaults()
         ->autowire()
-        ->autoconfigure()
-        ->public();
+        ->autoconfigure(); //->public();
 
     $NAMESPACE = BaksDevAuthYandexBundle::NAMESPACE;
     $PATH = BaksDevAuthYandexBundle::PATH;
